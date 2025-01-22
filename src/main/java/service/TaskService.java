9ea8c1fbab832da66e01f3598c832b823bcf42e3
kimseunghyun <incongnito12@gmail.com>
2025-01-22 09:43:14 +0900

    updated service to give required outputs

---
+++ src/main/java/service/TaskService.java
@@ -0,0 +1,55 @@
+package service;
+
+import entity.Task;
+import repository.TaskRepository;
+
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public String markDone(int index){
+        try {
+            Task selectedTask = taskRepository.getById(index-1);
+            if(selectedTask.getCompleted()) {
+                return "the task is already marked as done \n" + selectedTask + "  ->  " + selectedTask;
+            } else {
+                String response = "the task has been marked as done \n" + selectedTask;
+                selectedTask.toggleCompleted();
+                response = response + "  ->  " + selectedTask;
+                return response;
+            }
+        } catch (IndexOutOfBoundsException e) {
+            return "the requested index" + index + "is out of bounds";
+        }
+    }
+
+    public String markUndone(int index){
+        try {
+            Task selectedTask = taskRepository.getById(index-1);
+            if(selectedTask.getCompleted()) {
+                String response = "the task has been marked as undone \n" + selectedTask;
+                selectedTask.toggleCompleted();
+                response = response + "  ->  " + selectedTask;
+                return response;
+            } else {
+                return "the task is already marked as undone \n" + selectedTask + "  ->  " + selectedTask;
+            }
+        } catch (IndexOutOfBoundsException e) {
+            return "the requested index" + index + "is out of bounds";
+        }
+    }
+
+    public String getAllTasks(){
+        return taskRepository.getAll();
+    }
+
+    public String addTask(String taskName){
+        Task newTask = new Task(taskName);
+        String response = taskRepository.store(newTask);
+        String header = "Nice! I have added this task \n";
+        return header + response + "\n";
+    }
+}
