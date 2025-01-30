ac4cd6184dbd85700c5d41d2b3315ced9d1d23d0
kimseunghyun <incongnito12@gmail.com>
2025-01-31 03:55:55 +0900

    add markdirty and additional dependency to new interface for file buffer -> note that this has now become strongly coupled to the individual repositories -> may be best to have a separate service layer managing this in the future

--- src/main/java/service/ITaskService.java
+++ src/main/java/service/ITaskService.java
@@ -1,4 +1,21 @@
 package service;
 
-public interface ITaskService {
+import DIContainer.AOPInterfaces.AnnotationInterfaces.ExceptionHandler;
+import DIContainer.AOPInterfaces.AnnotationInterfaces.ProxyEnabled;
+import DIContainer.Proxiable;
+
+import java.util.List;
+
+@ProxyEnabled(implementation = TaskService.class)
+public interface ITaskService extends Proxiable {
+    @ExceptionHandler
+    String markDone(int index);
+    @ExceptionHandler
+    String markUndone(int index);
+    @ExceptionHandler
+    String getAllTasks();
+    @ExceptionHandler
+    String addTask(List<String> taskParams);
+    @ExceptionHandler
+    String deleteTask(int taskId);
 }
--- src/main/java/service/TaskService.java
+++ src/main/java/service/TaskService.java
@@ -1,62 +1,85 @@
 package service;
 
-import DIContainer.Proxiable;
 import entity.TaskType;
 import entity.tasks.Task;
 import entity.tasks.TaskFactory;
+import exceptions.UserFacingException;
+import repository.IFileBackedTaskRepository;
 import repository.ITaskRepository;
 
 import java.util.List;
 
-public class TaskService implements Proxiable {
+public class TaskService implements ITaskService {
     private final ITaskRepository taskRepository;
+    private final IFileBackedTaskRepository taskBuffer;
 
-    public TaskService(ITaskRepository taskRepository) {
+    public TaskService(ITaskRepository taskRepository, IFileBackedTaskRepository taskBuffer) {
         this.taskRepository = taskRepository;
+        this.taskBuffer = taskBuffer;
     }
 
-    public String markDone(int index){
-        try {
-            Task selectedTask = taskRepository.getById(index-1);
-            if(selectedTask.getCompleted()) {
-                return "the task is already marked as done \n" + selectedTask + "  ->  " + selectedTask;
-            } else {
-                String response = "the task has been marked as done \n" + selectedTask;
-                selectedTask.toggleCompleted();
-                response = response + "  ->  " + selectedTask;
-                return response;
-            }
-        } catch (IndexOutOfBoundsException e) {
-            return "the requested index" + index + "is out of bounds";
+    public String markDone(int index) {
+        Task selectedTask = taskRepository.findById(index - 1).orElseThrow(()->new UserFacingException("Task not found"));
+        if(selectedTask == null) {
+            throw new UserFacingException("Task not found");
+        }
+        if (selectedTask.getCompleted()) {
+            return "the task is already marked as done \n" + selectedTask + "  ->  " + selectedTask;
+        } else {
+            String response = "the task has been marked as done \n" + selectedTask;
+            selectedTask.toggleCompleted();
+            taskBuffer.markDirty(selectedTask.getId());
+            response = response + "  ->  " + selectedTask;
+            return response;
         }
     }
 
-    public String markUndone(int index){
-        try {
-            Task selectedTask = taskRepository.getById(index-1);
-            if(selectedTask.getCompleted()) {
-                String response = "the task has been marked as undone \n" + selectedTask;
-                selectedTask.toggleCompleted();
-                response = response + "  ->  " + selectedTask;
-                return response;
-            } else {
-                return "the task is already marked as undone \n" + selectedTask + "  ->  " + selectedTask;
-            }
-        } catch (IndexOutOfBoundsException e) {
-            return "the requested index" + index + "is out of bounds";
+    public String markUndone(int index) {
+
+        Task selectedTask = taskRepository.findById(index - 1).orElseThrow(()->new UserFacingException("Task not found"));
+        if (selectedTask.getCompleted()) {
+            String response = "the task has been marked as undone \n" + selectedTask;
+            selectedTask.toggleCompleted();
+            taskBuffer.markDirty(selectedTask.getId());
+            response = response + "  ->  " + selectedTask;
+            return response;
+        } else {
+            return "the task is already marked as undone \n" + selectedTask + "  ->  " + selectedTask;
         }
     }
 
-    public String getAllTasks(){
-        return taskRepository.getAll();
+    public String getAllTasks() {
+        List<Task> tasks = taskRepository.findAll();
+        int counter = 1;
+        StringBuilder result = new StringBuilder();
+        for(Task word : tasks) {
+            result.append(counter).append(".").append(word).append("\n");
+            counter++;
+        }
+        return result.toString();
     }
 
-    public String addTask(List<String> taskParams){
-        TaskType taskType = TaskType.valueOf(taskParams.getFirst().toUpperCase());
-        taskParams.removeFirst();
+    public String addTask(List<String> taskParams) {
+        TaskType taskType = TaskType.valueOf(taskParams.get(0).toUpperCase());
+        taskParams.remove(0);
         Task newTask = TaskFactory.createTask(taskType, taskParams);
-        String response = taskRepository.store(newTask);
-        String header = "Nice! I have added this task \n";
+        Task response = taskRepository.save(newTask);
+
+        String header = "Nice! I have added this task \n added: ";
         return header + response + "\n";
+    }
+
+    @Override
+    public String deleteTask(int taskId) {
+        Task deleted = taskRepository.deleteById(taskId-1);
+        Integer remainingTasks = taskRepository.remainingTasks();
+        StringBuilder result = new StringBuilder();
+        result.append("the following task has been deleted\n")
+                .append(deleted)
+                .append("\n you have ")
+                .append(remainingTasks)
+                .append(" tasks in the list");
+
+        return result.toString();
     }
 }
